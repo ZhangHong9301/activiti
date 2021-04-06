7e59830664d45c042397306dabf1ce698b55eedc
ZhangHong9301 <1253993436@qq.com>
2021-04-06 15:46:52 +0800

    添加描述

--- src/main/java/com/bonc/activiti/core/UUIDGenerator.java
+++ src/main/java/com/bonc/activiti/core/UUIDGenerator.java
@@ -10,6 +10,20 @@ import java.util.UUID;
 
 /**
  * @Description:
+ * UidGenerator是百度开源的Java语言实现，基于Snowflake算法的唯一ID生成器。
+ * 而且，它非常适合虚拟环境，比如：Docker。另外，它通过消费未来时间克服了雪花算法的并发限制。
+ * UidGenerator提前生成ID并缓存在RingBuffer中。 压测结果显示，单个实例的QPS能超过6000,000。
+ * 依赖环境：
+ * JDK8+
+ * MySQL（用于分配WorkerId）
+ *
+ * snowflake
+ * 由下图可知，雪花算法的几个核心组成部分：
+ * 1位sign标识位​；
+ * 41位时间戳；
+ * 10位workId（数据中心+工作机器，可以其他组成方式）；
+ * 12位自增序列；
+ * 
  * @Author: ZhangHong
  * @Date: 2019-12-24 16:31
  */
